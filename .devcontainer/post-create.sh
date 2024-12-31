5bd9db8103660ee2935fb0678afc9bad80797f67
Baalakay <BlakeTMcDonald@gmail.com>
2024-12-31 18:22:44 -0500

    map local .zsh_profile to container .zsh_profile

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,16 +1,13 @@
-#!/usr/bin/env bash
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-# nvm install 23
-# node -v
-# npm -v
-#export VSCODE_WORKDIR = $PWD
 pip install devtools
 pip install pydantic
 pip install logfire
 pip install mypy
 pip install "fastapi[standard]"
-python pip install -U pymupdf
-python pip install -U openai
+pip install -U pymupdf
+pip install pymupdf4llm
+pip install -U openai
+pip install pydantic-ai
+pip install 'pydantic-ai[logfire]'
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
@@ -19,11 +16,9 @@ git clone https://github.com/Baalakay/.dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 stow --adopt .
 git reset --hard
-pip install pydantic-ai
-pip install 'pydantic-ai[logfire]'
-# pip install 'pydantic-ai[examples]'
 cd $LOCAL_WORKSPACE_FOLDER
 #echo $cd $VSCODE_WORKDIR
-zsh
-sudo chsh -s $(which zsh) $(whoami)
-source ~/.zshrc
+#zsh
+#sudo chsh -s $(which zsh) $(whoami)
+
+
